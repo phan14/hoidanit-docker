fc81ec3b666f9d8230d38f448fe086d1396b54d0
đình trần <trandangdinh@dtu.edu.vn>
2022-08-19 08:44:58 +0700

    ss53

--- my-app/src/components/Header/Header.jsx
+++ my-app/src/components/Header/Header.jsx
@@ -26,6 +26,8 @@ const Header = () => {
             </NavLink>
           </Nav>
           <Nav>
+            <button className="btn-login">Log in</button>
+            <button className="btn-logout">Log out</button>
             <NavDropdown title="Settings" id="basic-nav-dropdown">
               <NavDropdown.Item>Log in</NavDropdown.Item>
               <NavDropdown.Item>Log out</NavDropdown.Item>
